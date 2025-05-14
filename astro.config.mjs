c698952e99e07094170eeee42aa8e43163ca5f73
Josh Dunsterville <neversitdull@icloud.com>
2025-05-14 15:59:00 -0700

    Add tailwind and experimental font api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        name: "Archivo",
+        cssVariable: "--font-archivo",
+        provider: fontProviders.fontsource(),
+        subsets: ["latin"],
+        variationSettings: "'wdth' [62.5, 75, 87.5, 100, 112.5, 125]",
+        weights: ["100 900"],
+      },
+    ],
+  },
+});
